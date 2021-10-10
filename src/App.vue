137346ab57a2e1b32587797c8418170a67257a22
Laís Rhianne <lays.rhianne@gmail.com>
2021-10-10 14:37:37 -0300

    error handling added to project

--- src/App.vue
+++ src/App.vue
@@ -13,23 +13,47 @@
 
     <v-main>
       <Home />
+      <v-snackbar
+        v-model="errorSnackbarStatus"
+        color="primary"
+        :multi-line="true"
+      >
+        {{ errorSnackbarMessage }}
+
+        <template v-slot:action="{ attrs }">
+          <v-btn
+            color="white"
+            text
+            v-bind="attrs"
+            @click="errorSnackbarStatus = false"
+          >
+            Close
+          </v-btn>
+        </template>
+      </v-snackbar>
     </v-main>
   </v-app>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
-import Home from "@/pages/Home.vue";
+import Vue from 'vue';
+import Home from '@/pages/Home.vue';
 
 export default Vue.extend({
-  name: "App",
+  name: 'App',
 
   components: {
     Home,
   },
 
   data: () => ({
-    //
+    errorSnackbarStatus: false,
+    errorSnackbarMessage: '',
   }),
+
+  errorCaptured(err, vm, info) {
+    this.errorSnackbarMessage = err.message;
+    this.errorSnackbarStatus = true;
+  },
 });
 </script>
