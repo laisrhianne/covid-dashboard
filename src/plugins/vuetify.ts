947b20cfa86df05a6df3d99e53ce3817528d78cd
Laís Rhianne <lays.rhianne@gmail.com>
2021-10-10 12:05:26 -0300

    theme added to vuetify

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -3,4 +3,19 @@ import Vuetify from "vuetify/lib/framework";
 
 Vue.use(Vuetify);
 
-export default new Vuetify({});
+export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: "#fe0002",
+        secondary: "#424242",
+        opposite: "#099631",
+        accent: "#FF5C5C",
+        error: "#FF5252",
+        info: "#2196F3",
+        success: "#4CAF50",
+        warning: "#FFC107",
+      },
+    },
+  },
+});
